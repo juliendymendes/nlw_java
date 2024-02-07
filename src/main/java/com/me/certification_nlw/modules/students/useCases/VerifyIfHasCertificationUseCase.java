4455e91416863e6ffa5d0e61861c84f906788bb6
Juliendy Mendes <juliendy0708@gmail.com>
2024-02-07 08:49:34 -0400

    continuação aula 2

--- src/main/java/com/me/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/me/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,12 +1,18 @@
 package com.me.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.me.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.me.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
-	public boolean execute(VerifyHasCertificationDTO dto){ 
+	@Autowired
+	private CertificationStudentRepository certificationStudentRepository;
+
+	public boolean execute(VerifyHasCertificationDTO dto){
+		this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
 		if(dto.getEmail().equals("aa@gmail.com") && dto.getTechnology().equals("JAVA")){
 			return true;
 		} 
